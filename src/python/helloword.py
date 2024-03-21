e461ec37d6f809ed999b3d08e99b0795ed857d70
elurisri <116026660+elurisri@users.noreply.github.com>
2024-03-21 06:01:18 +0530

    Delete src/python/helloword.py

--- src/python/helloword.py
+++
@@ -1,8 +0,0 @@
-python_distribution(
-  name="helloworld-dist",
-  description=env("DIST_DESC", "Set the `DIST_DESC` env variable to override this value."),
-  provides=python_artifact(
-    name="helloworld",
-    version=env("HELLO_WORLD_VERSION"),
-  ),
-)
